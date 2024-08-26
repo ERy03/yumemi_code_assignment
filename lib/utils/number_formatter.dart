61a6ac323d4023a13e64623f96aa9735b28455c4
Ryota <ryoenoki2013@gmail.com>
2024-08-26 17:06:31 +0900

    add: utils number formatter

---
+++ lib/utils/number_formatter.dart
@@ -0,0 +1,7 @@
+import 'package:intl/intl.dart';
+
+// RepositoryのStar数をカンマを使ってフォーマットするためのメソッド
+String numberWithComma(int num) {
+  final formatter = NumberFormat("###,###", "en_US");
+  return formatter.format(num);
+}
