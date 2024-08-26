bc6881fae9f348e699fc5c9a8f2f25950c7577d6
Ryota <ryoenoki2013@gmail.com>
2024-08-27 02:25:34 +0900

    add: locale keys

---
+++ lib/generated/locale_keys.g.dart
@@ -0,0 +1,17 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const find = 'find';
+  static const searchGH = 'searchGH';
+  static const searchRepository = 'searchRepository';
+  static const appLanguage = 'appLanguage';
+  static const lightMode = 'lightMode';
+  static const darkMode = 'darkMode';
+  static const noRepositories = 'noRepositories';
+  static const somethingWentWrong = 'somethingWentWrong';
+  static const rateLimitExceeded = 'rateLimitExceeded';
+  static const serviceUnavailable = 'serviceUnavailable';
+  static const networkError = 'networkError';
+  static const notFound = 'notFound';
+
+}
