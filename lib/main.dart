ab764e0b37de712bc92c5d0051f97b70e14e8517
Ryota <ryoenoki2013@gmail.com>
2024-08-26 23:41:20 +0900

    update: utilize theme and override sharedPreferencesProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:yumemi_code_assignment/presentation/screens/repositories_search_screen.dart';
+import 'package:yumemi_code_assignment/theme/theme_mode_provider.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  runApp(ProviderScope(overrides: [
+    // ここでUnimplementedErrorを実際のSharedPreferencesのインスタンスと上書きする
+    sharedPreferencesProvider.overrideWithValue(prefs),
+  ], child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeModeState = ref.watch(themeModeProvider);
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: themeModeState ? ThemeData.dark() : ThemeData.light(),
       home: const RepositoriesSearchScreen(),
     );
   }
