1f5ff41cb2d96304261ebe2662609db8c27fe03d
Ryota <ryoenoki2013@gmail.com>
2024-08-27 22:06:13 +0900

    update: RepositoryOverview

--- lib/presentation/components/shared/repository_overview.dart
+++ lib/presentation/components/shared/repository_overview.dart
@@ -3,24 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yumemi_code_assignment/data/github_language_colors_repository.dart';
 import 'package:yumemi_code_assignment/domain/github_repository_model.dart';
+import 'package:yumemi_code_assignment/utils/github_launch_url.dart';
 import 'package:yumemi_code_assignment/utils/number_formatter.dart';
 import 'package:yumemi_code_assignment/utils/string_to_color.dart';
 
 class RepositoryOverview extends ConsumerWidget {
   const RepositoryOverview({
     required this.gitHubRepositoryModel,
+    this.isDetailScreen = false,
     super.key,
   });
 
   final GitHubRepositoryModel gitHubRepositoryModel;
+  final bool isDetailScreen;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    const verticalSpacing = SizedBox(
-      height: 5,
+    final verticalSpacing = SizedBox(
+      height: isDetailScreen ? 8 : 5,
     );
-    const horizontalSpacing = SizedBox(
-      width: 3,
+    final horizontalSpacing = SizedBox(
+      width: isDetailScreen ? 10 : 3,
     );
 
     final colorList = ref.watch(getLanguageColorsProvider);
@@ -30,56 +33,66 @@ class RepositoryOverview extends ConsumerWidget {
       bottom: false,
       child: SizedBox(
         child: Container(
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(
-                color: Theme.of(context).dividerColor,
-                width: 0,
-              ),
-            ),
-          ),
+          decoration: isDetailScreen
+              ? null
+              : BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Theme.of(context).dividerColor,
+                      width: 0,
+                    ),
+                  ),
+                ),
           margin: const EdgeInsets.only(left: 16),
           padding: const EdgeInsets.only(top: 16, bottom: 16, right: 16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               // ユーザーのロゴとユーザーネーム
-              Row(
-                children: <Widget>[
-                  CachedNetworkImage(
-                    height: 32,
-                    width: 32,
-                    imageUrl: gitHubRepositoryModel.owner.avatarUrl,
-                    imageBuilder: (context, imageProvider) => Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          image: imageProvider,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    placeholder: (context, url) => Container(
+              GestureDetector(
+                onTap: () => isDetailScreen
+                    ? gitHubLaunchUrl(
+                        gitHubRepositoryModel.owner.htmlUrl,
+                        context,
+                      )
+                    : null,
+                child: Row(
+                  children: <Widget>[
+                    CachedNetworkImage(
                       height: 32,
                       width: 32,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.grey,
+                      imageUrl: gitHubRepositoryModel.owner.avatarUrl,
+                      imageBuilder: (context, imageProvider) => Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: imageProvider,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      placeholder: (context, url) => Container(
+                        height: 32,
+                        width: 32,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Flexible(
+                      child: Text(
+                        gitHubRepositoryModel.owner.login,
+                        overflow: TextOverflow.ellipsis,
                       ),
                     ),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Flexible(
-                    child: Text(
-                      gitHubRepositoryModel.owner.login,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
 
               verticalSpacing,
@@ -89,8 +102,11 @@ class RepositoryOverview extends ConsumerWidget {
                 gitHubRepositoryModel.name,
                 maxLines: 1,
                 overflow: TextOverflow.ellipsis,
-                style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                style: TextStyle(
+                  fontWeight:
+                      isDetailScreen ? FontWeight.w800 : FontWeight.bold,
+                  fontSize: isDetailScreen ? 22 : 16,
+                ),
               ),
 
               verticalSpacing,
@@ -101,7 +117,42 @@ class RepositoryOverview extends ConsumerWidget {
                 visible: gitHubRepositoryModel.description != null,
                 child: Text(
                   gitHubRepositoryModel.description ?? '',
-                  style: const TextStyle(fontSize: 16),
+                  style: TextStyle(fontSize: isDetailScreen ? 17 : 16),
+                ),
+              ),
+
+              verticalSpacing,
+
+              // Detail screenではwebsiteに遷移できる
+              Visibility(
+                visible: isDetailScreen &&
+                    gitHubRepositoryModel.homepage != null &&
+                    gitHubRepositoryModel.homepage != '',
+                child: Row(
+                  children: <Widget>[
+                    const Icon(
+                      Icons.link,
+                      size: 18,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Flexible(
+                      child: GestureDetector(
+                        onTap: () async {
+                          await gitHubLaunchUrl(
+                            gitHubRepositoryModel.homepage!,
+                            context,
+                          );
+                        },
+                        child: Text(
+                          gitHubRepositoryModel.homepage ?? '',
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontSize: 17),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
 
@@ -116,13 +167,20 @@ class RepositoryOverview extends ConsumerWidget {
                     size: 18,
                   ),
                   horizontalSpacing,
-                  Text(
-                    numberWithComma(gitHubRepositoryModel.stargazersCount),
-                    style: const TextStyle(fontSize: 18),
-                  ),
+                  if (isDetailScreen)
+                    Text(
+                      // ignore: lines_longer_than_80_chars
+                      "${numberCompact(gitHubRepositoryModel.stargazersCount)} ${gitHubRepositoryModel.stargazersCount == 1 ? "Star" : "Stars"}",
+                      style: const TextStyle(fontSize: 18),
+                    )
+                  else
+                    Text(
+                      numberWithComma(gitHubRepositoryModel.stargazersCount),
+                      style: const TextStyle(fontSize: 18),
+                    ),
 
-                  const SizedBox(
-                    width: 15,
+                  SizedBox(
+                    width: isDetailScreen ? 20 : 15,
                   ),
 
                   // プログラミング言語
