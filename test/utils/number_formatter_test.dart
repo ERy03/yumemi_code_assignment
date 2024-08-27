cc67721eb664f549cbd45fef14d6060dcc317ac8
Ryota <ryoenoki2013@gmail.com>
2024-08-27 23:24:29 +0900

    update: include numberCompact test

--- test/utils/number_formatter_test.dart
+++ test/utils/number_formatter_test.dart
@@ -16,4 +16,18 @@ void main() {
       expect(numberWithComma(0), '0');
     });
   });
+
+  group('numberCompact', () {
+    test('formats number compactly correctly', () {
+      expect(numberCompact(1000), '1K');
+      expect(numberCompact(1200000), '1.2M');
+      expect(numberCompact(987654321), '988M');
+    });
+
+    test('formats small numbers without compact notation correctly', () {
+      expect(numberCompact(123), '123');
+      expect(numberCompact(45), '45');
+      expect(numberCompact(0), '0');
+    });
+  });
 }
