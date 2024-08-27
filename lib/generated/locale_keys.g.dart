6a7d6499b94322dfcc22abd0384233001e55f684
Ryota <ryoenoki2013@gmail.com>
2024-08-27 22:03:11 +0900

    update: translation keys

--- lib/generated/locale_keys.g.dart
+++ lib/generated/locale_keys.g.dart
@@ -1,6 +1,16 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
 abstract class LocaleKeys {
+  static const issues = 'issues';
+  static const forks = 'forks';
+  static const watchers = 'watchers';
+  static const license = 'license';
+  static const viewOnGitHub = 'viewOnGitHub';
+  static const shareVia = 'shareVia';
+  static const none = 'none';
+  static const error = 'error';
+  static const unableToLaunchUrl = 'unableToLaunchUrl';
+  static const ok = 'ok';
   static const find = 'find';
   static const searchGH = 'searchGH';
   static const searchRepository = 'searchRepository';
